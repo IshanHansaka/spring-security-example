37f019695f80438ed09d7ae7e2139d6f2d914738
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-14 09:23:51 +0530

    feat: print session id and change default username and password

--- src/main/java/com/telusko/spring_security_example/controller/HelloController.java
+++ src/main/java/com/telusko/spring_security_example/controller/HelloController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/")
 public class HelloController {
 
     @GetMapping
-    public String welcome() {
-        return "welcome to home page!";
+    public String welcome(HttpServletRequest request) {
+        return "welcome to home page! " + request.getSession().getId();
     }
 }
