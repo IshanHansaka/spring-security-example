33f1320c79b70ac9fb32541520cfa3c64c00a728
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-14 10:21:22 +0530

    feat: create student model

---
+++ src/main/java/com/telusko/spring_security_example/model/Student.java
@@ -0,0 +1,16 @@
+package com.telusko.spring_security_example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+    private int mark;
+    
+}
